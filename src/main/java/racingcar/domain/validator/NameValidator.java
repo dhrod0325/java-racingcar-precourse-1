bf1df27fe8e9f72c1c4e75b008bd1dbf663a9ece
설아아빠 <dhrod0325@naver.com>
2022-04-20 03:29:38 +0900

    refactor : rename

--- src/main/java/racingcar/domain/validator/NameValidator.java
+++ src/main/java/racingcar/domain/validator/NameValidator.java
@@ -5,16 +5,16 @@ import racingcar.domain.error.Error;
 public class NameValidator {
     public static final int MAX_CAR_NAME_LENGTH = 5;
 
-    public static final String ERROR_CAR_NAME_EMPTY = Error.msg("차 이름은 공백일 수 없습니다.");
-    public static final String ERROR_CAR_NAME_OVER_LENGTH = Error.msg("차 이름의 길이는 %d여야 합니다.");
+    public static final String MSG_ERROR_CAR_NAME_EMPTY = Error.msg("차 이름은 공백일 수 없습니다.");
+    public static final String MSG_ERROR_CAR_NAME_OVER_LENGTH = Error.msg("차 이름의 길이는 %d여야 합니다.");
 
     public static String validate(String name) {
         if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException(ERROR_CAR_NAME_EMPTY);
+            throw new IllegalArgumentException(MSG_ERROR_CAR_NAME_EMPTY);
         }
 
         if (name.length() > MAX_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException(String.format(ERROR_CAR_NAME_OVER_LENGTH, MAX_CAR_NAME_LENGTH));
+            throw new IllegalArgumentException(String.format(MSG_ERROR_CAR_NAME_OVER_LENGTH, MAX_CAR_NAME_LENGTH));
         }
 
         return name;
--- src/main/java/racingcar/domain/validator/NumberValidator.java
+++ src/main/java/racingcar/domain/validator/NumberValidator.java
@@ -3,7 +3,7 @@ package racingcar.domain.validator;
 import racingcar.domain.error.Error;
 
 public class NumberValidator {
-    public static final String ERROR_NUMBER = Error.msg("횟수는 정수로 입력하세요");
+    public static final String MSG_ERROR_NUMBER = Error.msg("횟수는 정수로 입력하세요");
 
     public static int validate(String number) {
         int result;
@@ -11,11 +11,11 @@ public class NumberValidator {
         try {
             result = Integer.parseInt(number);
         } catch (Exception e) {
-            throw new IllegalArgumentException(ERROR_NUMBER);
+            throw new IllegalArgumentException(MSG_ERROR_NUMBER);
         }
 
         if (result < 0) {
-            throw new IllegalArgumentException(ERROR_NUMBER);
+            throw new IllegalArgumentException(MSG_ERROR_NUMBER);
         }
 
         return result;
