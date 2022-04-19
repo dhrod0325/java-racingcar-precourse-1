0d606579e0465864cb378c95058cadd225b9c20c
설아아빠 <dhrod0325@naver.com>
2022-04-19 20:14:42 +0900

    refactor : 자동차 이름 공백 검사 추가

--- src/main/java/racingcar/domain/Validator.java
+++ src/main/java/racingcar/domain/Validator.java
@@ -2,12 +2,17 @@ package racingcar.domain;
 
 public class Validator {
     public static final String ERROR_PREFIX = "[ERROR]";
+    public static final String ERROR_CAR_NAME_EMPTY = ERROR_PREFIX + "차 이름은 공백일 수 없습니다.";
     public static final String ERROR_CAR_NAME_OVER_LENGTH = ERROR_PREFIX + "이름의 길이는 %d여야 합니다";
     public static final String ERROR_GAME_COUNT = ERROR_PREFIX + "횟수는 숫자로 입력하세요";
 
     public static final int MAX_CAR_NAME_LENGTH = 5;
 
     public static String validateCarName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_CAR_NAME_EMPTY);
+        }
+
         if (name.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(String.format(ERROR_CAR_NAME_OVER_LENGTH, MAX_CAR_NAME_LENGTH));
         }
