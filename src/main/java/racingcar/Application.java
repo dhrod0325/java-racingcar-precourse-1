7fd8855b8d1da52f6e5158552c0b671bf697dff5
설아아빠 <dhrod0325@naver.com>
2022-04-19 23:16:26 +0900

    refactor : 주석 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,19 @@
 package racingcar;
 
-import racingcar.game.controller.GameControllerBuilder;
 import racingcar.domain.status.RandomMoveStatus;
+import racingcar.game.controller.GameControllerBuilder;
 import racingcar.game.view.InputView;
 import racingcar.game.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        int RANDOM_CAR_MOVE_MIN = 0;
-        int RANDOM_CAR_MOVE_MAX = 9;
+        int RANDOM_CAR_FORWARD_MIN = 0;
+        int RANDOM_CAR_FORWARD_MAX = 9;
 
-        // TODO: 프로그램 구현
         new GameControllerBuilder()
                 .setInputView(new InputView())
                 .setOutputView(new OutputView())
-                .setMoveStatus(new RandomMoveStatus(RANDOM_CAR_MOVE_MIN, RANDOM_CAR_MOVE_MAX))
+                .setMoveStatus(new RandomMoveStatus(RANDOM_CAR_FORWARD_MIN, RANDOM_CAR_FORWARD_MAX))
                 .build()
                 .run();
     }
