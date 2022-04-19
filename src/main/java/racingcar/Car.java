7aab0ed88812c4731c9841f36a8175bea7dd3cbf
설아아빠 <dhrod0325@naver.com>
2022-04-19 12:59:10 +0900

    feature : 자동차 움직임 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,14 @@
 package racingcar;
 
 public class Car {
+    private final String name;
+    private int position = 0;
+
     private CarStatus status;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void setState(int number) {
         if (number >= 4) {
@@ -19,5 +26,18 @@ public class Car {
 
     public boolean isStop() {
         return status == CarStatus.STOP;
+    }
+
+    public int move() {
+        if (isForward()) {
+            position++;
+        }
+
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,16 +1,32 @@
 package racingcar;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+    Car car;
+
+    @BeforeEach
+    public void before() {
+        car = new Car("pobi");
+    }
+
     @Test
     public void 테스트_자동차상태테스트() {
-        Car car = new Car();
         car.setState(4);
         Assertions.assertTrue(car.isForward());
 
         car.setState(3);
         Assertions.assertTrue(car.isStop());
+    }
+
+    @Test
+    public void 테스트_자동차움직임() {
+        car.setState(4);
+        Assertions.assertEquals(car.move(), 1);
+
+        car.setState(3);
+        Assertions.assertEquals(car.move(), 1);
     }
 }
