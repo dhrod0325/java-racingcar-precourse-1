e889e1f0d3e4ad200adc8d8d3927fc99f9c800e8
설아아빠 <dhrod0325@naver.com>
2022-04-19 22:05:35 +0900

    refactor : 상수 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,23 @@
 package racingcar.view;
 
 public class OutputView {
+    public static final String MSG_WINNER = "최종 우승자는 %s 입니다.";
+    public static final String MSG_INPUT_COUNT = "시도할회수는몇회인가요?";
+    public static final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.( 이름은쉼표(,)기준으로구분)";
+
     public void display(String str) {
         System.out.println(str);
     }
 
     public void displayWinners(String winners) {
-        display(String.format("최종 우승자는 %s 입니다.", winners));
+        display(String.format(MSG_WINNER, winners));
     }
 
     public void displayInputCount() {
-        display("시도할회수는몇회인가요?");
+        display(MSG_INPUT_COUNT);
     }
 
     public void displayInputCarNames() {
-        display("경주할 자동차 이름을 입력하세요.( 이름은쉼표(,)기준으로구분)");
+        display(MSG_INPUT_CAR_NAMES);
     }
 }
