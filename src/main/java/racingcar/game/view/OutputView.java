3c22c459787158e06d34f48aa92885dc7fddc7cf
dhrod0325 <dhrod0325@naver.com>
2022-04-22 11:45:37 +0900

    fix : 결과 테스트 요구사항에 맞게 변경

--- src/main/java/racingcar/game/view/OutputView.java
+++ src/main/java/racingcar/game/view/OutputView.java
@@ -3,7 +3,7 @@ package racingcar.game.view;
 public class OutputView {
     public static final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.( 이름은쉼표(,)기준으로구분)";
     public static final String MSG_INPUT_COUNT = "시도할회수는몇회인가요?";
-    public static final String MSG_WINNER = "최종 우승자는 %s 입니다.";
+    public static final String MSG_WINNER = "최종 우승자 : %s";
 
     public void display(String str) {
         System.out.println(str);
